5979d5f2d1e0a954cfd3ef8248aa97004988387b
Charles Irvine <cr.irvine.kc@gmail.com>
2025-04-13 17:01:32 -0500

    updated index.tsx to show a RN Paper Card

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,8 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+
+import { Avatar, Button, Card, Text } from 'react-native-paper';
+
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
 export default function Index() {
   return (
@@ -9,7 +13,18 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Card>
+        <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+        <Card.Content>
+          <Text variant="titleLarge">Card title</Text>
+          <Text variant="bodyMedium">Card content</Text>
+        </Card.Content>
+        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
+        </Card.Actions>
+      </Card>
     </View>
   );
 }
