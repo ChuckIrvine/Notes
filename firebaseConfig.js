147a246110212609000cf7c3da70044f58155963
Charles Irvine <cr.irvine.kc@gmail.com>
2025-04-13 16:42:59 -0500

    initial commit after following the firebase setup from expo docs

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALP-cfc1cenpMbjO7-ai2yGC6_UuR_AS4",
+  authDomain: "notes-1be4e.firebaseapp.com",
+  projectId: "notes-1be4e",
+  storageBucket: "notes-1be4e.firebasestorage.app",
+  messagingSenderId: "878704222332",
+  appId: "1:878704222332:web:03a770bd014415acac69f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
